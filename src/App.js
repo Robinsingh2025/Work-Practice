05b44651ba44380045b91fe55aa615281f0de21a
Robin7374 <7374robin@gmail.com>
2019-10-11 16:10:13 -0600

    dev Ed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
 import React from 'react';
-import Todos from './components/Todos';
 
 function App() {
-  this.state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Dinner with girlly',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'meeting with boss',
-        completed: false
-      }
-    ]
-  };
-  console.log(this.state.todos);
-  return (
-    <div className='App'>
-      <Todos />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
